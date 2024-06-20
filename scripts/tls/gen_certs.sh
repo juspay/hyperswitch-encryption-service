69ebf5dab7260bc680481c75894e0ded01b4822d
dracarys18 <karthikey.hegde@juspay.in>
2024-06-20 12:24:57 +0530

    chore: add client certificate generation script

--- scripts/tls/gen_certs.sh
+++ scripts/tls/gen_certs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ALT="DNS:localhost"
 CA_SUBJECT="/C=US/ST=CA/O=Cripta CA/CN=Cripta CA"
 SUBJECT="/C=US/ST=CA/O=Cripta/CN=localhost"
@@ -12,6 +14,10 @@ function gen_ca_if_non_existent() {
   if ! [ -f ./ca_cert.pem ]; then gen_ca; fi
 }
 
+function gen_client_key() {
+    cat rsa_sha256_cert.pem rsa_sha256_key.pem  > client.pem
+}
+
 function gen_rsa_sha256() {
   gen_ca_if_non_existent
 
@@ -22,11 +28,9 @@ function gen_rsa_sha256() {
     -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial \
     -in server.csr -out rsa_sha256_cert.pem
 
-  openssl pkcs12 -export -password pass:rocket \
-    -in rsa_sha256_cert.pem -inkey rsa_sha256_key.pem -out rsa_sha256.p12
-
   rm ca_cert.srl server.csr
 }
 
 gen_rsa_sha256
+gen_client_key
 
