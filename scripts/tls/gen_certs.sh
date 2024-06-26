d8f62cf463bff8c0a3d3393d39264b7fbe382fba
dracarys18 <karthikey.hegde@juspay.in>
2024-06-26 18:21:45 +0530

    chore: make configurable cert generation script

--- scripts/tls/gen_certs.sh
+++ scripts/tls/gen_certs.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-ALT="DNS:localhost"
-CA_SUBJECT="/C=US/ST=CA/O=Cripta CA/CN=Cripta CA"
-SUBJECT="/C=US/ST=CA/O=Cripta/CN=localhost"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --prod)
+      PROD="true"
+      shift # past argument
+      ;;
+    --namespace)
+      NAMESPACE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --service)
+      SERVICE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$PROD" == "true" ]; then
+    ALT=$(printf "DNS:%s.%s.svc.cluster.local" $NAMESPACE $SERVICE)
+    CA_SUBJECT="/C=US/ST=CA/O=Cripta CA/CN=Cripta CA"
+    SUBJECT=$(printf "/C=US/ST=CA/O=Cripta/CN=%s.%s.svc.cluster.local" $NAMESPACE $SERVICE)
+else
+    ALT="DNS:localhost"
+    CA_SUBJECT="/C=US/ST=CA/O=Cripta CA/CN=Cripta CA"
+    SUBJECT="/C=US/ST=CA/O=Cripta/CN=localhost"
+fi
 
 function gen_ca() {
   openssl genrsa -out ca_key.pem 4096
