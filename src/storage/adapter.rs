e6dcbb9e06e48c0899fe16d0dd32c50584382297
dracarys18 <karthikey.hegde@juspay.in>
2024-12-09 12:59:46 +0530

    feat: make the storage layer pluginable

---
+++ src/storage/adapter.rs
@@ -0,0 +1,25 @@
+mod cassandra;
+mod postgres;
+
+use crate::{config::Config, errors, storage::DbState};
+
+pub struct PostgreSQL;
+pub struct Cassandra;
+
+pub trait DbAdapterType {}
+
+impl DbAdapterType for PostgreSQL {}
+impl DbAdapterType for Cassandra {}
+
+#[async_trait::async_trait]
+pub trait DbAdapter {
+    type Conn<'a>
+    where
+        Self: 'a;
+    type AdapterType: DbAdapterType;
+
+    async fn from_config(config: &Config) -> DbState<Self::AdapterType>;
+    async fn get_conn<'a>(
+        &'a self,
+    ) -> errors::CustomResult<Self::Conn<'a>, errors::ConnectionError>;
+}
