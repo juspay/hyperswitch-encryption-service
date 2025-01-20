b9ee005b6b56c2a60c6ecf9dcb4be1c0338ca3d1
Vaibhav Khinvasara <89930190+vkhinvasara@users.noreply.github.com>
2025-01-20 13:33:07 +0000

    bump version to 0.1.0 and update Postman requests for data encryption and decryption along with neccesary CI changes

--- postman/collection-dir/Encrypt Data/event.test.js
+++ postman/collection-dir/Encrypt Data/event.test.js
@@ -3,5 +3,4 @@ pm.test("Decrypt Data - Status code is 200", function () {
 });
 var response = pm.response.json();
 
-// Set the value of 'ff' to a variable named 'ff_value'
-pm.environment.set("ff_value", response.data.ff);
+pm.environment.set("value", response.data.value);
