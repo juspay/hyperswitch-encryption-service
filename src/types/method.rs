ed765f0fad5e2ac9d5a2a483780eea1bc1f98e23
vkhinvasara <vaibhavkhinvasara@gmail.com>
2025-02-01 03:24:55 +0530

    initial structure

--- src/types/method.rs
+++ src/types/method.rs
@@ -4,14 +4,13 @@ use crate::{
     multitenancy::TenantState,
     types::Identifier,
 };
-use futures::future;
 
 #[derive(Eq, PartialEq, Debug, serde::Serialize, serde::Deserialize, Clone)]
 #[serde(untagged)]
 pub enum EncryptionType {
     Single(super::DecryptedData),
     Batch(super::DecryptedDataGroup),
-    MultiBatch(Vec<super::DecryptedDataGroup>)
+    MultiBatch(super::MultipleDecryptionDataGroup)
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
@@ -19,39 +18,7 @@ pub enum EncryptionType {
 pub enum DecryptionType {
     Single(super::EncryptedData),
     Batch(super::EncryptedDataGroup),
-    MultiBatch(Vec<super::EncryptedDataGroup>)
-}
-
-pub struct MultiBatchProcessor;
-
-impl MultiBatchProcessor {
-    pub async fn decrypt(
-        data: Vec<super::EncryptedDataGroup>,
-        state: &TenantState,
-        identifier: &Identifier,
-        custodian: Custodian,
-    ) -> errors::CustomResult<EncryptionType, errors::CryptoError> {
-        let decrypted_data_groups = future::try_join_all(data.into_iter().map(|encrypted_data_group| {
-            encrypted_data_group.decrypt(state, identifier, custodian.clone())
-        }))
-        .await?;
-
-        Ok(EncryptionType::MultiBatch(decrypted_data_groups))
-    }
-
-    pub async fn encrypt(
-        data: Vec<super::DecryptedDataGroup>,
-        state: &TenantState,
-        identifier: &Identifier,
-        custodian: Custodian,
-    ) -> errors::CustomResult<DecryptionType, errors::CryptoError> {
-        let encrypted_data_groups = future::try_join_all(data.into_iter().map(|decrypted_data_group| {
-            decrypted_data_group.encrypt(state, identifier, custodian.clone())
-        }))
-        .await?;
-
-        Ok(DecryptionType::MultiBatch(encrypted_data_groups))
-    }
+    MultiBatch(super::MultipleEncryptionDataGroup)
 }
 
 impl DecryptionType {
@@ -69,7 +36,7 @@ impl DecryptionType {
                 EncryptionType::Batch(data.decrypt(state, identifier, custodian).await?)
             }
             Self::MultiBatch(data) => {
-                MultiBatchProcessor::decrypt(data, state, identifier, custodian).await?
+                EncryptionType::MultiBatch(data.decrypt(state, identifier, custodian).await?)
             }
         })
     }
@@ -89,8 +56,8 @@ impl EncryptionType {
             Self::Batch(data) => {
                 DecryptionType::Batch(data.encrypt(state, identifier, custodian).await?)
             }
-            Self::MultiBatch(data) =>{
-                MultiBatchProcessor::encrypt(data, state, identifier, custodian).await?
+            Self::MultiBatch(data) => {
+                DecryptionType::MultiBatch(data.encrypt(state, identifier, custodian).await?)
             }
         })
     }
