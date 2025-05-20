76ab407c6a07dfbb4bcd66e758e0059622c3a40a
Sanchith Hegde <sanchith.hegde@juspay.in>
2025-05-21 01:14:49 +0530

    chore: address Rust 1.87 clippy lints

--- src/app/tls.rs
+++ src/app/tls.rs
@@ -14,10 +14,7 @@ pub async fn from_config(config: &Config) -> io::Result<ServerConfig> {
 
     let priv_key =
         rustls_pemfile::private_key(&mut certs.tls_key.expose(config).await.peek().as_ref())?
-            .ok_or(io::Error::new(
-                io::ErrorKind::Other,
-                "Could not parse pem file",
-            ))?;
+            .ok_or(io::Error::other("Could not parse pem file"))?;
 
     let cert = cert.into_iter().map(CertificateDer::from).collect();
 
@@ -25,18 +22,18 @@ pub async fn from_config(config: &Config) -> io::Result<ServerConfig> {
 
     for ca in rustls_pemfile::certs(&mut certs.root_ca.expose(config).await.peek().as_ref()) {
         roots
-            .add(ca.map_err(|err| io::Error::new(io::ErrorKind::Other, err))?)
-            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+            .add(ca.map_err(io::Error::other)?)
+            .map_err(io::Error::other)?;
     }
 
     let auth = WebPkiClientVerifier::builder(Arc::new(roots))
         .build()
-        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+        .map_err(io::Error::other)?;
 
     let mut config = ServerConfig::builder()
         .with_client_cert_verifier(auth)
         .with_single_cert(cert, priv_key)
-        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+        .map_err(io::Error::other)?;
 
     config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
 
