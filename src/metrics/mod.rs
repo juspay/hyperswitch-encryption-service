427bdd9f13c425ee9b8a8a0b10f3fa97b7e1f78e
Sanchith Hegde <sanchith.hegde@juspay.in>
2025-12-08 15:50:37 +0530

    refactor(metrics): specify buckets for API latency metrics

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -1,6 +1,20 @@
 use once_cell::sync::Lazy;
 use opentelemetry::metrics::{Counter, Histogram, Meter};
 
+const fn duration_histogram_buckets() -> [f64; 25] {
+    let mut buckets = [0.0; 25];
+    let mut value = 1e-6; // 1 microsecond
+    let mut i = 0;
+
+    while i < 25 {
+        buckets[i] = value;
+        value *= 2.0;
+        i += 1;
+    }
+
+    buckets
+}
+
 pub(crate) static METER: Lazy<Meter> = Lazy::new(|| opentelemetry::global::meter("cripta"));
 
 pub(crate) static HEALTH_METRIC: Lazy<Counter<u64>> =
@@ -18,8 +32,16 @@ pub(crate) static KEY_CREATE_FAILURE: Lazy<Counter<u64>> =
 pub(crate) static KEY_ROTATE_FAILURE: Lazy<Counter<u64>> =
     Lazy::new(|| METER.u64_counter("KEY_ROTATE_FAILURE").build());
 
-pub(crate) static ENCRYPTION_API_LATENCY: Lazy<Histogram<f64>> =
-    Lazy::new(|| METER.f64_histogram("ENCRYPTION_API_LATENCY").build());
+pub(crate) static ENCRYPTION_API_LATENCY: Lazy<Histogram<f64>> = Lazy::new(|| {
+    METER
+        .f64_histogram("ENCRYPTION_API_LATENCY")
+        .with_boundaries(Vec::from(duration_histogram_buckets()))
+        .build()
+});
 
-pub(crate) static DECRYPTION_API_LATENCY: Lazy<Histogram<f64>> =
-    Lazy::new(|| METER.f64_histogram("DECRYPTION_API_LATENCY").build());
+pub(crate) static DECRYPTION_API_LATENCY: Lazy<Histogram<f64>> = Lazy::new(|| {
+    METER
+        .f64_histogram("DECRYPTION_API_LATENCY")
+        .with_boundaries(Vec::from(duration_histogram_buckets()))
+        .build()
+});
