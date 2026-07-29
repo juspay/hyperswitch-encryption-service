027e170a1041b99ca327165763df039adfcb808a
Sanchith Hegde <sanchith.hegde@juspay.in>
2026-07-30 03:01:38 +0530

    refactor(health): remove dependency on `AppState` in health handler

--- src/core/health.rs
+++ src/core/health.rs
@@ -1,12 +1,6 @@
-use std::sync::Arc;
+use crate::env::{metrics, observability as logger};
 
-use axum::extract::State;
-
-use crate::{app::AppState, env::observability as logger, metrics};
-
-pub(crate) async fn heath_check(
-    State(_): State<Arc<AppState>>,
-) -> (hyper::StatusCode, &'static str) {
+pub(crate) async fn heath_check() -> (hyper::StatusCode, &'static str) {
     logger::info!("Health was called");
     metrics::HEALTH_METRIC.add(1, &[]);
     (hyper::StatusCode::OK, "Health is good")
