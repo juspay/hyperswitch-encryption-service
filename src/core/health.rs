a1aa5d176f8d595a0b7c3fd2ccf4cf30b16f1cc1
Sanchith Hegde <sanchith.hegde@juspay.in>
2026-08-01 21:27:09 +0530

    chore: fix typo in health check handler name

--- src/core/health.rs
+++ src/core/health.rs
@@ -1,7 +1,7 @@
 use crate::env::observability as logger;
 
 #[tracing::instrument(skip_all)]
-pub(crate) async fn heath_check() -> (hyper::StatusCode, &'static str) {
+pub(crate) async fn health_check() -> (hyper::StatusCode, &'static str) {
     logger::info!("Health was called");
     (hyper::StatusCode::OK, "Health is good")
 }
